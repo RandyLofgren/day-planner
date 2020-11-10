77ed3b5397ce3f2d4126a54356f296160ba13d66
RandyLofgren <43276499+RandyLofgren@users.noreply.github.com>
2020-11-10 09:24:14 -0500

    saving text content

--- Assets/script.js
+++ Assets/script.js
@@ -1,6 +1,23 @@
-var saveButton = $("#ready")
+// var saveButton = $("#ready")
+// var clickedHour= $("img:focus").attr('data-hour');
 
-saveButton.on("click",function(){
-
-    alert(working)
+$(".saveBtn").on("click",function(){
+    var clickedHour = $(this).attr("data-hour")
+    var text = $("#hour"+clickedHour).val()
+   localStorage.setItem("time"+clickedHour, text)
 })
+
+// alert(clickedHour)
+    
+// })
+
+for (let i = 9; i < 18; i++) {
+    var hourText = localStorage.getItem("time"+i)
+    if(hourText !== null){
+        $("#hour"+i).val(hourText)
+    }
+    
+}
+
+// $("#hour"+clickedHour).val()
+
