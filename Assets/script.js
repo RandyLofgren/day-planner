0bd5f8efb826565027de7df3a1d9ca625cd2cc7e
RandyLofgren <43276499+RandyLofgren@users.noreply.github.com>
2020-11-11 17:32:01 -0500

    updated readme

--- Assets/script.js
+++ Assets/script.js
@@ -1,23 +1,22 @@
-// var saveButton = $("#ready")
-// var clickedHour= $("img:focus").attr('data-hour');
-
-$(".saveBtn").on("click",function(){
+$(".saveBtn").on("click", function () {
     var clickedHour = $(this).attr("data-hour")
-    var text = $("#hour"+clickedHour).val()
-   localStorage.setItem("time"+clickedHour, text)
+    var text = $("#hour" + clickedHour).val()
+    localStorage.setItem("time" + clickedHour, text)
 })
-
-// alert(clickedHour)
-    
-// })
-
+var now = moment()
 for (let i = 9; i < 18; i++) {
-    var hourText = localStorage.getItem("time"+i)
-    if(hourText !== null){
-        $("#hour"+i).val(hourText)
+    var currentHour = parseInt(now.format("H"))
+    console.log(currentHour)
+    if (i < currentHour) {
+        $("#hour" + i).addClass("past")
+    } else if (i === currentHour) {
+        $("#hour" + i).addClass("present")
+    } else {
+        $("#hour" + i).addClass("future")
     }
-    
+    var hourText = localStorage.getItem("time" + i)
+    if (hourText !== null) {
+        $("#hour" + i).val(hourText)
+    }
 }
-
-// $("#hour"+clickedHour).val()
-
+$("#currentDay").text(now.format("dddd, MMMM Do YYYY"))
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # day-planner
+
+## a work-day scheduler that stores user info
+
+## uses moment to keep track of current time and show upcoming events vs previous events
+
+
+
+https://randylofgren.github.io/day-planner/
+
+![2020-11-11 (2)](https://user-images.githubusercontent.com/43276499/98871934-bc919b80-2443-11eb-8e38-dd1a019990fa.png)
